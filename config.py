d937ef8adc0e6a8f4ddc8f6d6a4379577074beac
Flint15 <chesspace341@gmail.com>
2024-10-25 17:51:29 +0300

    Update config.py
    add some functions and call_words

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 from functions import *
 
+call_words = ['eva', 'say']
+
+opened_sites = {}
+pid = ''
+
 functions = {
-	'time': get_time
+	'time':  get_time,
+	'open':  open_site,
+	'close': close_site
+
 }
